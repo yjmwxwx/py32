4ced57fe3ed7ad2cfb0ef2ce333bafc76f92da10
yjmwxwx <yjmwxwx@yandex.com>
2023-04-16 16:53:33 +0800

    时钟设置

---
+++ rcc/1.s
@@ -0,0 +1,133 @@
+	@@ 单片机PY32F002AF15P6TU
+	@时钟设置
+	@时间：2023-04-16
+	@编译器：ARM-NONE-EABI
+	.thumb
+	.syntax unified
+	.section .text
+
+vectors:	
+	.word STACKINIT
+	.word _start + 1
+	.word _nmi_handler + 1
+	.word _hard_fault  + 1
+	.word 0
+	.word 0
+	.word 0
+	.word 0
+	.word 0
+	.word 0
+	.word 0
+	.word _svc_handler +1
+	.word 0
+	.word 0
+	.word _pendsv_handler +1
+	.word _systickzhongduan +1           @ 15
+	.word aaa +1     @ _wwdg +1          @ 0
+	.word aaa +1     @_pvd +1            @ 1
+	.word aaa +1     @_rtc +1            @ 2
+	.word aaa +1     @_flash +1          @ 3
+	.word aaa +1    @ _rcc + 1           @ 4
+	.word aaa +1      @_exti0_1  +1      @ 5
+	.word aaa +1      @ _exti2_3 +1      @ 6
+	.word aaa +1       @_exti4_15 +1     @ 7
+	.word aaa +1                         @ 8
+	.word aaa +1    @__dma_wan  +1       @ 9
+	.word aaa +1    @_dma1_2_3 +1        @ 10
+	.word aaa +1       @_dma1_4_5 +1     @ 11
+	.word aaa +1     @_adc1 +1           @ 12
+	.word aaa +1       @_tim1_brk_up +1  @ 13
+	.word aaa +1        @ _tim1_cc +1    @ 14
+	.word aaa +1         @_tim2 +1       @ 15
+	.word aaa +1          @_tim3 +1      @ 16
+	.word aaa +1     @LPTIM1               @ 17
+	.word aaa +1                         @ 18
+	.word aaa +1    @_tim14 +1           @ 19
+	.word aaa +1                         @ 20
+	.word aaa +1         @_tim16 +1      @ 21
+	.word aaa +1         @_tim17 +1      @ 22
+	.word aaa +1          @_i2c   +1     @ 23
+	.word aaa +1                         @ 24
+	.word aaa +1           @_spi1  +1    @ 25
+	.word aaa +1           @_spi2  +1    @ 26
+	.word aaa +1         @_usart1 +1     @ 27
+	.word aaa +1	@_usart2 +1	     @ 28
+	.word aaa +1				@29
+	.word aaa +1	@led			@30
+	.word aaa +1				@31
+
+_start:
+shizhong:
+	ldr r2, = 0x40022000   @FLASH访问控制
+	movs r1, # 1
+	str r1, [r2]           @flash 读操作有 1 个等待状态
+	ldr r0, = 0x40021000 @ rcc
+	ldr r1, = 0x20008
+	str r1, [r0, # 0x10]	@外部晶振选择
+	ldr r1, [r0]
+	ldr r2, = 0x10000
+	orrs r1, r1, r2
+	str r1, [r0]		@开外部振荡器
+denghse:
+	ldr r1, [r0]
+	lsls r1, r1, # 14
+	bpl denghse		@等外部振荡器
+	
+	ldr r1, = 0x01
+	str r1, [r0, # 0x0c]	@PLL配置
+	ldr r2, [r0]
+	ldr r1, = 0x1000000
+	orrs r1, r1, r2
+	str r1, [r0]		@开PLL
+
+dengpll:
+	ldr r1, [r0]
+	lsls r1, # 6
+	bpl dengpll		@等PLL
+	movs r1, # 0x02
+	str r1, [r0, # 0x08]	@选择系统时钟
+
+
+kaishi:
+__kai_pa_shi_zhong:
+	ldr r0, = 0x40021034
+	movs r1, # 1
+	str r1, [r0]
+__pa_she_zhi:
+	ldr r0, = 0x50000000
+	ldr r1, = 0xebfffffD
+	str r1, [r0]
+	movs r1, # 1
+ting:
+__led_kai:
+	str r1, [r0, # 0x18]
+	ldr r2, = 0xfffff
+__led_kai_yanshi:
+	subs r2, r2, # 1
+	bne __led_kai_yanshi
+__led_guan:
+	str r1, [r0, # 0x28]
+	ldr r2, = 0xfffff
+__led_guan_yanshi:
+	subs r2, r2, # 1
+	bne __led_guan_yanshi
+	b ting
+
+
+_nmi_handler:
+	bx lr
+_hard_fault:
+	bx lr
+_svc_handler:
+	bx lr
+_pendsv_handler:
+	bx lr
+_systickzhongduan:
+
+aaa:
+	bx lr
+	.ltorg
+	.section .data
+	.align 4
+	.equ STACKINIT,		0x20001000
+	.end
